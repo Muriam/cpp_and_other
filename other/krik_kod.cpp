9506a5543536ec5c44cf9318a4acc220862312cd
Maria Duvanova <d_muriam@inbox.ru>
2025-11-19 17:22:52 +0700

    Update krik_kod.cpp
    edit file for compilation

--- other/krik_kod.cpp
+++ other/krik_kod.cpp
@@ -1,23 +1,32 @@
-#Include <iostream>
-
+#include <iostream>
+ 
 #define a int
 #define aa float  
 #define aaa main
-#define aaaaa (
-#define aaaaa )
-#define aaaaaaa {
-#define aaaaaaa }
-#define aaaaaaaaa ;
+#define aaaa (         
+#define aaaaa )        
+#define aaaaaa {       
+#define aaaaaaa }      
+#define aaaaaaaa ;     
 #define aaaaaaaaa return
 #define aaaaaaaaaa for
 #define aaaaaaaaaaa while
-#define aaaaaaaaaaa if
-#define aaaaaaaaaaaaaa std
-
-a aaa aaaa aaaaa
-
-a i = 1 aaaaaaaa
-
-aaaaaaaaaaa aaaa i=1 aaaaaa
-
-std:icout << "hello" aaaaaaaa
+#define aaaaaaaaaaaa if
+#define aaaaaaaaaaaaa std
+ 
+// --- ИСПРАВЛЕНИЕ #1: Добавлен недостающий макрос 'aaaaa' для закрывающей скобки main() ---
+a aaa aaaa aaaaa aaaaaa                  // int main() { 
+ 
+a i = 1 aaaaaaaa                         // int i = 1;
+ 
+// --- ИСПРАВЛЕНИЕ #2 (логика): Используется i == 1 (сравнение) вместо i = 1 (присваивание) ---
+aaaaaaaaaaa aaaa i == 1 aaaaa            // while (i == 1) 
+aaaaaa                                   // {
+ 
+aaaaaaaaaaaaa::cout << "hello" aaaaaaaa  // std::cout << "hello";
+ 
+i = 2 aaaaaaaa;                          // Добавлена строка, чтобы цикл не был бесконечным
+ 
+aaaaaaa                                  // }
+aaaaaaaa return 0 aaaaaaaa;              // return 0;
+aaaaaaa                                  // }
