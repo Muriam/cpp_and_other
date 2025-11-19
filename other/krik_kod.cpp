894f979b921aeddeda84f87ecb8c00979004f62c
Muriam <d_muriam@inbox.ru>
2025-11-19 11:53:54 +0700

    aaa

---
+++ other/krik_kod.cpp
@@ -0,0 +1,23 @@
+#Include <iostream>
+
+#define a int
+#define aa float  
+#define aaa main
+#define aaaaa (
+#define aaaaa )
+#define aaaaaaa {
+#define aaaaaaa }
+#define aaaaaaaaa ;
+#define aaaaaaaaa return
+#define aaaaaaaaaa for
+#define aaaaaaaaaaa while
+#define aaaaaaaaaaa if
+#define aaaaaaaaaaaaaa std
+
+a aaa aaaa aaaaa
+
+a i = 1 aaaaaaaa
+
+aaaaaaaaaaa aaaa i=1 aaaaaa
+
+std:icout << "hello" aaaaaaaa
